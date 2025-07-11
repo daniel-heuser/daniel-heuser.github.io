1785860345d451bdccc61fdf950706a7b5ccfbc0
Daniel Heuser <hello@danielheuser.com>
2025-07-12 00:01:08 +0200

    feat: update site URL to danielheuser.com and add CNAME file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://daniel-heuser.github.io',
-  base: '/',
-  output: 'static',
-  build: {
-    assets: 'assets'
-  }
+    site: 'https://danielheuser.com',
+    base: '/',
+    output: 'static',
+    build: {
+        assets: 'assets'
+    }
 });
